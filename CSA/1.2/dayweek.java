37baa3aabdbe8d4b4b91f8ff1cd629cb30f646fb
VoidVex5 <104126091+VoidVex5@users.noreply.github.com>
2022-09-04 13:00:22 +0300

    Update dayweek.java

--- CSA/1.2/dayweek.java
+++ CSA/1.2/dayweek.java
@@ -5,9 +5,12 @@ public class dayweek {
         int y = Integer.parseInt(args[2]);
 
         int y0 = y - (14 - m) / 12;
+        // calculates year
         int x = y0 + y0/4 - y0/100 + y0/400;
         int m0 = m + 12 * ((14 - m) / 12) - 2;
+        // calculates month
         int d0 = (d + x + (31*m0)/12) % 7;
+        // calculates day
 
         System.out.println(d0);
     }
