a3c133f99e3113abfc6498e95b6c31ae75d64778
VoidVex5 <104126091+VoidVex5@users.noreply.github.com>
2022-09-04 13:28:46 +0300

    Update dragon_interpreter.java

--- CSA/1.2/dragon_interpreter.java
+++ CSA/1.2/dragon_interpreter.java
@@ -1,16 +1,42 @@
 public class dragon_interpreter {
     public static void main(String[] args) {
         String dragon0 = "F";
+        // one forward
         String nogard0 = "F";
+        // one forward
         String dragon1 = dragon0 + "L" + nogard0;
+        // one forwards, left, one forwards
         String nogard1 = dragon0 + "R" + nogard0;
+        // one forward, right, one forward
         String dragon2 = dragon1 + "L" + nogard1;
+        // one forwards, left, one forwards, left, one forward, right, one forward
         String nogard2 = dragon1 + "R" + nogard1;
+        // one forwards, left, one forwards, right, one forward, right, one forward
         String dragon3 = dragon2 + "L" + nogard2;
+        // one forwards, left, one forwards, left, one forward, right, one forward, left, 
+        // one forwards, left, one forwards, right, one forward, right, one forward
         String nogard3 = dragon2 + "R" + nogard2;
+        // one forwards, left, one forwards, left, one forward, right, one forward, right,
+        // one forwards, left, one forwards, right, one forward, right, one forward
         String dragon4 = dragon3 + "L" + nogard3;
+        // one forwards, left, one forwards, left, one forward, right, one forward, left, 
+        // one forwards, left, one forwards, right, one forward, right, one forward, left,
+        // one forwards, left, one forwards, left, one forward, right, one forward, right,
+        // one forwards, left, one forwards, right, one forward, right, one forward
         String nogard4 = dragon3 + "R" + nogard3;
+        // one forwards, left, one forwards, left, one forward, right, one forward, left, 
+        // one forwards, left, one forwards, right, one forward, right, one forward, right,
+        // one forwards, left, one forwards, left, one forward, right, one forward, right,
+        // one forwards, left, one forwards, right, one forward, right, one forward
         String dragon5 = dragon4 + "L" + nogard4;
+        // one forwards, left, one forwards, left, one forward, right, one forward, left, 
+        // one forwards, left, one forwards, right, one forward, right, one forward, left,
+        // one forwards, left, one forwards, left, one forward, right, one forward, right,
+        // one forwards, left, one forwards, right, one forward, right, one forward, left
+        // one forwards, left, one forwards, left, one forward, right, one forward, left, 
+        // one forwards, left, one forwards, right, one forward, right, one forward, right,
+        // one forwards, left, one forwards, left, one forward, right, one forward, right,
+        // one forwards, left, one forwards, right, one forward, right, one forward
 
         System.out.println(dragon0);
         System.out.println(dragon1);
