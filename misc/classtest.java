b70bf980a669f55a3e76799eaa35343a6e8fc372
VoidVex5 <104126091+VoidVex5@users.noreply.github.com>
2022-09-16 12:02:58 +0300

    comments

--- misc/classtest.java
+++ misc/classtest.java
@@ -3,33 +3,34 @@ class cat {
 	private double weight;
 	private String name;
 	private boolean asleep;
+	// attributes
 
 	public cat() {
 		name = new String("unnamed");
 		age = 6;
 		weight = 4.5;
 		
-	}
+	} // default constructor
 
 	public cat(int a, double w, String n, boolean as) {
 		age = a;
 		weight = w;
 		name = n;
 		asleep = as;
-	}
+	} // regular constructor
 
 	public void meow(double db) {
 		System.out.println("Meow!");
-	}
+	} // meow method
 
-	public boolean isAsleep() { return asleep; }
+	public boolean isAsleep() { return asleep; } // checks if the cat is asleep
 	
-	public double weigh_cat() { return weight; }
+	public double weigh_cat() { return weight; } // weighs the cat and gets its weight
 }
 
-public class classtest {
+public class classtest { // main classs
 	public static void main(String[] args) {
-		cat Cat = new cat(12, 3.7, "Boncuk", false);
-		cat u_cat = new cat(); 
+		cat Cat = new cat(12, 3.7, "Boncuk", false); // an intentional cat
+		cat u_cat = new cat(); // default cat
 	}
 }
